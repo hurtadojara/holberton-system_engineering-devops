7b11e2d1dc13d9e128dbd260d9a14d425da4465f
hurtadojara <andreshurtadojaramillo@gmail.com>
2020-12-22 00:00:12 -0500

    server de mierdaaaaaaaaaaaaaa

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,8 @@
+# Tcustom HTTP header X-Served-By
+exec { 'Custom_header_response':
+    command  => 'sudo apt-get update;
+    sudo apt-get -y install nginx;
+    sudo sed -i "18i add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-enabled/default;
+    sudo service nginx restart',
+    provider => 'shell'
+}
